bed4d94e3a869f6f63567ceb940fad6cbac02ff0
lulu <lucybtf@gmail.com>
2016-05-03 17:14:25 +0200

    Creación de ficheros de las pantallas Biblioteca, ObjetoExcavado, Otros, Pecio.

---
+++ app/src/main/java/com/example/artilleriaapp/artilleriaapp/ObjetoExcavado.java
@@ -0,0 +1,13 @@
+package com.example.artilleriaapp.artilleriaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ObjetoExcavado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objeto_excavado);
+    }
+}
---
+++ app/src/main/java/com/example/artilleriaapp/artilleriaapp/Pecio.java
@@ -0,0 +1,13 @@
+package com.example.artilleriaapp.artilleriaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pecio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pecio);
+    }
+}
