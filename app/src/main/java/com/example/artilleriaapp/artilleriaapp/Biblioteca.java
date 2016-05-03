20f6e05b6a3be0a8cb45b7c945a5e8c18028420b
lulu <lucybtf@gmail.com>
2016-05-03 17:09:47 +0200

    Creación del la Pantalla de Registro de Biblioteca .

---
+++ app/src/main/java/com/example/artilleriaapp/artilleriaapp/Biblioteca.java
@@ -0,0 +1,13 @@
+package com.example.artilleriaapp.artilleriaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Biblioteca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biblioteca);
+    }
+}
